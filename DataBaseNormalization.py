d4567378250fa87a5c179a0f2d26f8d0558efcb1
Bonse <84657555+lxthxrgx@users.noreply.github.com>
2023-03-15 11:19:10 +0300

    Update DataBaseNormalization.py

--- DataBaseNormalization.py
+++ DataBaseNormalization.py
@@ -8,8 +8,9 @@ cursor = sqlite_connection.cursor()
 
 workbook = load_workbook(r'excel\excel sources\montransactions.xlsx')
 sheet = workbook['123']
-
-cursor.execute('''CREATE TABLE IF NOT EXISTS mon (
+cursor.execute('DROP TABLE IF EXISTS mon')
+cursor.execute('''CREATE TEMPORARY TABLE mon (
+    Id INTEGER PRIMARY KEY,
 	Cudnum	TEXT,
 	Koatyy	TEXT,
 	Area	TEXT,
@@ -54,71 +55,27 @@ for i in range(2, 470864):
      o = sheet.cell(row=i, column = 16).value
      p = sheet.cell(row=i, column = 17).value
      q = sheet.cell(row=i, column = 18).value
-     cursor.execute( "INSERT INTO mon (Cudnum,Koatyy,Area,District,Settlement,City,Street,TGName,PurposeOfTheAssignment,NameOfTheSite,TransactionType,Price,OwnershipType,RegistrationDate,RegistrationNumber,ValueNGO,EvaluationDate) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",(a,b,c,d,e,f,g,h,x,j,k,l,m,n,o,p,q,))
+     cursor.execute('''INSERT INTO mon (
+        Cudnum,
+        Koatyy,
+        Area,
+        District,
+        Settlement,
+        City,
+        Street,
+        TGName,
+        PurposeOfTheAssignment,
+        NameOfTheSite,
+        TransactionType,
+        Price,
+        OwnershipType,
+        RegistrationDate,
+        RegistrationNumber,
+        ValueNGO,
+        EvaluationDate) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''',(a,b,c,d,e,f,g,h,x,j,k,l,m,n,o,p,q,))
 
 sqlite_connection.commit()
 workbook.close()
-
-cursor.execute('''CREATE TEMPORARY TABLE mon_temp (
-    Cudnum	TEXT,
-	Koatyy	TEXT,
-	Area	TEXT,
-	District	TEXT,
-	Settlement	TEXT,
-	City	TEXT,
-	Street	TEXT,
-	TGName	TEXT,
-	PurposeOfTheAssignment	TEXT,
-    PurposeOfTheAssignmentNormal	TEXT,
-	NameOfTheSite	TEXT,
-	TransactionType	TEXT,
-	Price	TEXT,
-	OwnershipType	TEXT,
-	RegistrationDate	TEXT,
-	RegistrationNumber	TEXT,
-	ValueNGO	TEXT,
-	EvaluationDate	TEXT
-)''')
-
-cursor.execute('''INSERT INTO mon_temp(
-    `Cudnum`,
-	`Koatyy`,
-	`Area`,
-	`District`,
-	`Settlement`,
-	`City`,
-	`Street`,
-	`TGName`,
-	`PurposeOfTheAssignment`,
-    `PurposeOfTheAssignmentNormal`,
-	`NameOfTheSite`,
-	`TransactionType`,
-	`Price`,
-	`OwnershipType`,
-	`RegistrationDate`,
-	`RegistrationNumber`,
-	`ValueNGO`,
-	`EvaluationDate`
-) SELECT 
-    `Cudnum`,
-	`Koatyy`,
-	`Area`,
-	`District`,
-	`Settlement`,
-	`City`,
-	`Street`,
-	`TGName`,
-	`PurposeOfTheAssignment`,
-    `PurposeOfTheAssignmentNormal`,
-	`NameOfTheSite`,
-	`TransactionType`,
-	`Price`,
-	`OwnershipType`,
-	`RegistrationDate`,
-	`RegistrationNumber`,
-	`ValueNGO`,
-	`EvaluationDate` FROM mon''')
-
 
 cursor.execute('CREATE TEMPORARY TABLE "dictionary" (fcdata	NUMERIC, nmdata	NUMERIC)')
 cursor.execute('INSERT INTO dictionary(fcdata, nmdata) SELECT PurposeOfTheAssignment, NULL FROM mon')
@@ -143,6 +100,7 @@ for i, a in enumerate(float_normalized_data_from_file_list[1:], start=2):
     cursor.execute(f"UPDATE dictionary SET nmdata = ? WHERE ROWID = {i};", (a,))
 
 cursor.execute("""CREATE TEMPORARY TABLE mon2(
+    Id INTEGER PRIMARY KEY,
 	Cudnum TEXT,
 	Koatyy TEXT,
 	Area TEXT,
@@ -164,6 +122,7 @@ cursor.execute("""CREATE TEMPORARY TABLE mon2(
 )""")
 
 cursor.execute("""INSERT INTO mon2(
+    Id ,
     Cudnum,
     Koatyy,
     Area,
@@ -181,6 +140,7 @@ cursor.execute("""INSERT INTO mon2(
     RegistrationNumber,
     ValueNGO,
     EvaluationDate) SELECT 
+    Id ,
     Cudnum,
     Koatyy,
     Area,
@@ -198,7 +158,7 @@ cursor.execute("""INSERT INTO mon2(
     RegistrationNumber,
     ValueNGO,
     EvaluationDate
-    FROM mon_temp""")
+    FROM mon""")
 
 cursor.execute('CREATE TABLE IF NOT EXISTS monitoring AS SELECT * FROM mon2')
 
@@ -207,35 +167,33 @@ cursor.execute('''UPDATE monitoring SET PurposeOfTheAssignmentNormal = (SELECT n
 reader.close()
 
 cursor.execute('SELECT PurposeOfTheAssignmentNormal,COUNT(PurposeOfTheAssignmentNormal), Area, Price, ValueNGO, AVG(Price/Area) AS Pa, AVG(Price/ValueNGO) AS Pv FROM monitoring GROUP BY PurposeOfTheAssignmentNormal')
-
 rows = cursor.fetchall()
-for row in rows:
-    purpose = row[0]
-    count = row[1]
-    area = row[2]
-    price = row[3]
-    value = row[4]
-    print(purpose, count, area, price, value)
 
-workbook = load_workbook('F:\Prog\Py\excel\excel draft\opus3.xlsx')
+
+workbook = load_workbook('F:\Prog\Py\excel\excel draft\ltx.xlsx')
 sheet = workbook['123']
 
 row_num = 8
 for row in rows:
     purpose = row[0]
-    area = row[1]
-    price = row[2]
-    value = row[3]
-    pa = row[4]
-    pv = row[5]
+    count = row[1]
+    price = row[3]
+    pa = row[5]
+    pv = row[6]
+
     sheet.cell(row=row_num, column=2).value = purpose
-    sheet.cell(row=row_num, column=5).value = area
+    sheet.cell(row=row_num, column=3).value = count
     sheet.cell(row=row_num, column=6).value = price
-    sheet.cell(row=row_num, column=7).value = value
-    sheet.cell(row=row_num, column=8).value = pa
-    sheet.cell(row=row_num, column=9).value = pv
+    sheet.cell(row=row_num, column=5).value = pa
+    sheet.cell(row=row_num, column=7).value = pv
+
     row_num += 1
 
-workbook.save('F:\Prog\Py\excel\excel draft\opus3.xlsx')
+
+workbook.save('F:\Prog\Py\excel\excel draft\ltx.xlsx')
+
+
+cursor.close()
+
 sqlite_connection.commit()
 cursor.close()
